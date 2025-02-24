afbdb55604f1365f6136d717303c47786f2241cf
fernando24164 <fernando24164@gmail.com>
2025-02-24 21:39:00 +0100

    fix: Redis test

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,9 +20,14 @@ def test_app():
 
     def get_test_redis_client():
         client_mock = AsyncMock(spec=redis.Redis)
-        client_mock.get = AsyncMock(side_effect=lambda key: b"product_123" if key == "last_viewed:123" else None)
+        client_mock.get = AsyncMock(
+            side_effect=lambda key: b"product_123" if key == "last_viewed:123" else None
+        )
         client_mock.rpush = AsyncMock(side_effect=lambda key, value: True)
         client_mock.ltrim = AsyncMock(side_effect=lambda key, start, stop: True)
+        client_mock.pipeline = Mock(
+            side_effect=lambda: AsyncMock(side_effect=lambda: True)
+        )
         return client_mock
 
     redis_mock = AsyncMock(spec=redis.ConnectionPool)
