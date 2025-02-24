ddd31626fd548083e270b1bbae3a127b557fd56a
fernando24164 <fernando24164@gmail.com>
2025-02-24 21:35:30 +0100

    fix: Ingestion service

--- app/services/ingestion_service.py
+++ app/services/ingestion_service.py
@@ -9,5 +9,8 @@ async def ingest_data_service(
     await opensearch_client.index(
         index="ingestion", body=data.model_dump(), refresh=True
     )
-    await redis_client.rpush(f"last_viewed:{data.customer_id}", data.product_id)
-    await redis_client.ltrim(f"last_viewed:{data.customer_id}", -10, -1)
+
+    pipeline = redis_client.pipeline()
+    pipeline.rpush(f"last_viewed:{data.customer_id}", data.product_id)
+    pipeline.ltrim(f"last_viewed:{data.customer_id}", 0, 9)
+    await pipeline.execute()
