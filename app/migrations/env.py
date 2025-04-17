15dffd0aa5ec688c18b6143932c36213124927b3
fernando24164 <fernando24164@gmail.com>
2025-04-17 19:54:58 +0200

    feat: Change migrations to adapt new data model

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -1,14 +1,15 @@
 import asyncio
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from alembic import context
+
 from core.config import get_settings
 from models import Base
-from models.web_event import *
+from models.game_store import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
