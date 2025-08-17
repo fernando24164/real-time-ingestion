29b4d043896738558f6d579f1466d69154d4e512
fernando24164 <fernando24164@gmail.com>
2025-08-17 20:17:46 +0200

    fix: refactor init.sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,9 +1,8 @@
--- Create game_store schema if it doesn't exist
-BEGIN
-IF NOT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'game_store') THEN
-CREATE SCHEMA game_store;
-RAISE NOTICE 'Created schema game_store';
-ELSE
-RAISE NOTICE 'Schema game_store already exists';
-END IF;
-END;
+-- This script is executed by the official Postgres Docker entrypoint
+-- on first initialization when the data directory is empty.
+
+-- Create schema if it doesn't exist (safe to run multiple times).
+CREATE SCHEMA IF NOT EXISTS game_store AUTHORIZATION postgres;
+
+-- Optional: document the schema
+COMMENT ON SCHEMA game_store IS 'Schema for game store domain';
